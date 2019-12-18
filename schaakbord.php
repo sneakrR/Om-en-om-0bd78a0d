970cf8d1c90e595e88bd0f50656a244915f9e6cc
Josh <2107767@talnet.nl>
2019-12-18 11:17:24 +0100

    Uitwerking van de exercise

---
+++ schaakbord.php
@@ -0,0 +1,135 @@
+<html>
+<head>
+
+<style type="text/css">
+
+th,
+td {
+        border: 0px solid rgb(0, 0, 0);
+        padding: 36px;
+	width: 10px;
+	text-align: center;
+}
+
+td {
+	text-align: center;
+	background-color: #404040;
+	color: #000000;
+	font-size: 15px;
+}
+
+#white {
+	background-color: white;
+	color: #000000;
+}
+
+#black {
+    background-color: black;
+    color: #000000;
+}
+
+table {
+	border-collapse: collapse;
+	border:2px solid rgb(200, 200, 200,);
+	letter-spacing: 1px;
+	font-family: sans-serif;
+	font-size: .8rem;
+	text-align: center;
+}
+</style>
+
+</head>
+
+<body>
+
+<table>
+        <canvas id="canvas" width="1" height="1"></canvas>
+        <tr>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+        </tr>
+        <tr>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+        </tr>
+        <tr>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+        </tr>
+        <tr>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+        </tr>
+        <tr>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+        </tr>
+        <tr>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+        </tr>
+        <tr>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+        </tr>
+        <tr>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+                <th id="black"></th>
+                <th id="white"></th>
+        </tr>
+</table>
+
+
+</body>
+</html>    
+
+
+<?php
+
